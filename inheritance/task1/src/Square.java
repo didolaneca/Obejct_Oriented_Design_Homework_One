a444eb2fa5c640423fde1ec3141aa7edb71c3643
Diyan Simeonov <diyan_simeonov@yahoo.com>
2021-01-21 23:35:08 -0600

    All Test passing for Homework One

--- inheritance/task1/src/Square.java
+++ inheritance/task1/src/Square.java
@@ -2,3 +2,43 @@
  Add variables if you need.
  Remember to use the @Override to override methods.
 intelliJ will be helpful if you do).*/
+public class Square extends Shape {
+    private final double sideLength;
+    public Square(double sideLength, String name){
+        super(name);
+        this.sideLength = sideLength;
+    }
+
+    @Override
+    public double getPerimeter() {
+        return 4 * this.sideLength;
+    }
+
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if(!(obj instanceof Square)){
+            return false;
+        }
+        Square other = (Square) obj;
+
+        return this.sideLength == other.sideLength && name.equals(other.getName());
+    }
+
+    @Override
+    public String toString() {
+        return new StringBuilder(this.name)
+                .append(":")
+                .append(this.sideLength)
+                .toString();
+    }
+
+    public double getArea(){
+        return this.sideLength * this.sideLength;
+    }
+
+}
