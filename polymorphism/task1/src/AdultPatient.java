24dab064f7fb46893342d30d932e0709c3344a70
Diyan Simeonov <diyan_simeonov@yahoo.com>
2021-01-26 20:21:06 -0600

    Task 2 solved all test passing

--- polymorphism/task1/src/AdultPatient.java
+++ polymorphism/task1/src/AdultPatient.java
@@ -1,5 +1,51 @@
 import java.util.ArrayList;
+// TODO
+// Implement the class Adult Patient.
+// An adult patient is a Person that is Diagnosticable
+// and that is Employable.
+public class AdultPatient extends Person implements Diagnosticable, Employable{
 
-public class AdultPatient/* TODO */
-Implement the class Adult Patient. 
-An adult patient is a Person that is Diagnosticable and that is Employable.
+    private int systolic, diastolic;
+    private double temperature;
+    private ArrayList<String> previousJobs = new ArrayList<>();
+    @Override
+    public double getTemperature() {
+        return this.temperature;
+    }
+
+    @Override
+    public int getSystolic() {
+        return this.systolic;
+    }
+
+    @Override
+    public int getDiastolic() {
+        return this.diastolic;
+    }
+
+
+    @Override
+    public void setTemperature(double t) {
+        this.temperature = t;
+    }
+
+    @Override
+    public void setSystolic(int systolic) {
+        this.systolic = systolic;
+    }
+
+    @Override
+    public void setDiastolic(int diastolic) {
+        this.diastolic = diastolic;
+    }
+
+    @Override
+    public ArrayList<String> getPreviousJobs() {
+        return this.previousJobs;
+    }
+
+    @Override
+    public void addJob(String job) {
+        this.previousJobs.add(job);
+    }
+}
--- polymorphism/task1/src/Diagnosticable.java
+++ polymorphism/task1/src/Diagnosticable.java
@@ -1,12 +1,19 @@
 public interface Diagnosticable {
     double getTemperature();
-/* TODO */
-Declare a hasFever method that reutrns true is the temperature
-is greater than 100.4
+/* TODO
+Declare a hasFever method that returns true if the temperature
+is greater than 100.4 */
+    default boolean hasFever(){
+        return getTemperature() > 100.4;
+    }
     int getSystolic();
     int getDiastolic();
     void setTemperature(double t);
-/* TODO */
-Decleare two setters. One for systolic and one for diastolic. 
+/* TODO
+Declare two setters. One for systolic and one for diastolic.
 Check the getters to infer the parameter types.
+ */
+    void setSystolic(int systolic);
+    void setDiastolic(int diastolic);
+
 }
